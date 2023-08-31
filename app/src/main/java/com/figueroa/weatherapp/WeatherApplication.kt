e69a4cfe325e3e98c71d8772a247b403b01737ef
FigueroaGit <hmfp24@gmail.com>
2023-08-31 15:33:16 -0600

    Setting up packages and DI structures

---
+++ app/src/main/java/com/figueroa/weatherapp/WeatherApplication.kt
@@ -0,0 +1,7 @@
+package com.figueroa.weatherapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApplication : Application()
---
+++ app/src/main/java/com/figueroa/weatherapp/di/AppModule.kt
@@ -0,0 +1,3 @@
+package com.figueroa.weatherapp.di
+
+class AppModule
